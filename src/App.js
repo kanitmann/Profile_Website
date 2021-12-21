22f039e1b80819ce725387783692e34783c71742
kanitmann <kanitmann90@gmail.com>
2021-12-21 22:13:30 +0530

    Added Logo, Powerbutton and Social Icons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import GlobalStyle from './globalStyles';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme } from './components/themes';
+import {ThemeProvider} from 'styled-components';
+import {darkTheme, lightTheme} from './components/themes';
+import {Route, Switch} from 'react-router-dom';
+import Home from './components/Home/home.js';
+import aboutMe from './components/AboutMe/aboutMe.js';
+import blogPage from './components/BlogPage/blogPage.js';
+import contactPage from './components/ContactMe/contactMe.js';
+import skills from './components/mySkills/myskill.js';
+import myWork from './components/workPage/workPage.js';
+import footer from './components/Footer/footer.js';
 
 function App() {
-  return <>
-    <GlobalStyle />
-    <ThemeProvider theme={lightTheme}>
-      My Files
-    </ThemeProvider>
-  </>
+    return <>
+        <GlobalStyle/>
+        <ThemeProvider theme={lightTheme}>
+            <Switch>
+                <Route exact path="/" component={Home}/>;
+                <Route exact path="/aboutme" component={aboutMe}/>;
+                <Route exact path="/blog" component={blogPage}/>;
+                <Route exact path="/contact" component={contactPage}/>;
+                <Route exact path="/skills" component={skills}/>;
+                <Route exact path="/mywork" component={myWork}/>;
+                <Route exact path="/footer" component={footer}/>;
+            </Switch>
+        </ThemeProvider>
+    </>
 }
 
 export default App;
