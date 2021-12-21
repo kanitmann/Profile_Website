c9fe48d87217963b1b60dfe6d9957b94beb2d00b
kanitmann <kanitmann90@gmail.com>
2021-12-21 12:53:10 +0530

    Setting environment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import GlobalStyle from './globalStyles';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from './components/themes';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <>
+    <GlobalStyle />
+    <ThemeProvider theme={lightTheme}>
+      My Files
+    </ThemeProvider>
+  </>
 }
 
 export default App;
