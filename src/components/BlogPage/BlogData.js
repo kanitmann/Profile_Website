2a42735a7a1011fe913fdc7e6ddc12a46818c819
kanitmann <kanitmann90@gmail.com>
2021-12-23 17:50:20 +0530

    Blog Page Added

---
+++ src/components/BlogPage/BlogData.js
@@ -0,0 +1,36 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"Unreleased",
+        tags:["Unreleased","Unreleased","Unreleased"],
+        date:"Coming Soon",
+        imgSrc:"https://images.pexels.com/photos/10167648/pexels-photo-10167648.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        link:"#"
+    },
+    {
+        id:2,
+        name:"Unreleased II",
+        tags:["Unreleased","Unreleased","Unreleased"],
+        date:"Coming Soon",
+        imgSrc:"https://images.pexels.com/photos/9443251/pexels-photo-9443251.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        link:"#"
+    },
+    {
+        id:3,
+        name:"Unreleased III",
+        tags:["Unreleased","Unreleased","Unreleased"],
+        date:"Coming Soon",
+        imgSrc:"https://images.pexels.com/photos/10167648/pexels-photo-10167648.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        link:"#"
+    },
+    {
+        id:4,
+        name:"Unreleased IV",
+        tags:["Unreleased","Unreleased","Unreleased"],
+        date:"Coming Soon",
+        imgSrc:"https://images.pexels.com/photos/9443251/pexels-photo-9443251.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        link:"#"
+    },
+    
+  
+] 
